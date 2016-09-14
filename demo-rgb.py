6958652df491691f623a43ee5c2e92de27a32276
Pavol Rusnak <stick@gk2.sk>
2016-09-14 22:28:22 +0200

    update examples

--- demo-rgb.py
+++ demo-rgb.py
@@ -1,4 +1,5 @@
 import machine
+import time
 
 led_r = machine.PWM(machine.Pin(15, machine.Pin.OUT), freq=1000, duty=0)
 led_g = machine.PWM(machine.Pin(12, machine.Pin.OUT), freq=1000, duty=0)
@@ -6,3 +7,16 @@ led_b = machine.PWM(machine.Pin(13, machine.Pin.OUT), freq=1000, duty=0)
 
 button = machine.Pin(4, machine.Pin.IN)
 photo = machine.ADC(0)
+
+t = 0
+
+while True:
+    # read button and photoresistor states
+    print('button:', button.value(), 'photo:', photo.read())
+    # set led colors
+    led_r.duty( int(math.cos(0.3 * t) * 450 + 500) )
+    led_g.duty( int(math.cos(0.5 * t) * 450 + 500) )
+    led_b.duty( int(math.cos(0.7 * t) * 450 + 500) )
+    # wait 10 ms
+    time.sleep(0.01)
+    t += 0.01
--- demo-wifi.py
+++ demo-wifi.py
@@ -8,7 +8,7 @@ nic.connect('SSID', 'password')
 addr = socket.getaddrinfo('ctjb.net', 80)[0][-1]
 sock = socket.socket()
 sock.connect(addr)
-sock.send(b'GET /2016 HTTP/1.1\r\nHost: ctjb.net\r\n\r\n')
+sock.send(b'GET / HTTP/1.1\r\nHost: ctjb.net\r\n\r\n')
 data = sock.recv(1024)
 sock.close()
 data = data.decode('ascii')
